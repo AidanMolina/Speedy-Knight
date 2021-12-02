8499a6141ccf6e8b5f1992825c68e8d635a549e8
apmolina <apmolina@iu.edu>
2021-12-02 00:27:44 -0500

    Player attack

--- Speedy Knight/Assets/PlayerPlatformerController.cs
+++ Speedy Knight/Assets/PlayerPlatformerController.cs
@@ -4,21 +4,32 @@ using UnityEngine;
 
 public class PlayerPlatformerController : PhysicsObject
 {
+    int health = 3;
+    public float jumpsLeft = 0f;
+    float attackPower = 1f;
+
     public float currentSpeed = 5f;
-    public float maxSpeed = 10f;
+    public float maxSpeed = 8f;
     public float jumpTakeOffSpeed = 3f;
 
     bool ticked;
     float tick = 0.5f;
 
+    bool attacking;
+    float attackTimer = 0.5f;
+
     private SpriteRenderer spriteRenderer;
     private Animator animator;
+    private BoxCollider2D boxCollider;
 
     void Awake(){
         spriteRenderer = GetComponent<SpriteRenderer>();
         animator = GetComponent<Animator>();
+        boxCollider = GetComponent<BoxCollider2D>();
         ticked = false;
+        attacking = false;
     }
+
 
     protected override void ComputeVelocity(){
         Vector2 move = Vector2.zero;
@@ -42,10 +53,10 @@ public class PlayerPlatformerController : PhysicsObject
             currentSpeed = 5f;
         }
 
-        if(Input.GetButtonDown("Jump") && grounded){
+        if(Input.GetKeyDown(KeyCode.W) && grounded){
             velocity.y = jumpTakeOffSpeed * currentSpeed/3;
         }
-        else if(Input.GetButtonUp("Jump")){
+        else if(Input.GetKeyUp(KeyCode.W)){
             if(velocity.y > 0f){
                 velocity.y = velocity.y * 0.5f;
             }
@@ -60,7 +71,27 @@ public class PlayerPlatformerController : PhysicsObject
         animator.SetFloat("velocityX", Mathf.Abs(velocity.x) / currentSpeed);
         
         targetVelocity = move * currentSpeed;
-        Debug.Log(currentSpeed);
 
+
+        if(Input.GetButtonDown("Jump") && attacking == false){
+            boxCollider.enabled = !boxCollider.enabled;
+            attacking = true;
+        }
+
+        if(attacking == true){
+            attackTimer -= Time.deltaTime;
+            if(attackTimer <= 0.0f){
+                boxCollider.enabled = !boxCollider.enabled;
+                attacking = false;
+                attackTimer = 0.5f;
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collider){
+        if(collider.gameObject.tag == "Enemy"){
+            collider.gameObject.GetComponent<Enemy>().health -= attackPower * currentSpeed;
+            Debug.Log(collider.gameObject.GetComponent<Enemy>().health);
+        }
     }
 }
